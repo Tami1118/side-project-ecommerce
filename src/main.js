1996c751c62cf61bd03c70d7c73dd33f8fbf56e7
Tami1118 <chenziyi93326@gmail.com>
2024-03-19 12:15:09 +0800

    :triangular_flag_on_post: Add: fontawesom plugin

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
 // 設定 vee validation 全域規則
 configure({
   generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
-  validateOnInput: true, // 輸入時，是否需同時執行驗證
+  validateOnInput: false, // 輸入時，是否需同時執行驗證
 });
 setLocale('zh_TW'); // 設定預設語系
 
@@ -40,7 +40,13 @@ setLocale('zh_TW'); // 設定預設語系
 // CKEditor
 import CKEditor from '@ckeditor/ckeditor5-vue'
 
-
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas, fab, far);
 
 // 註冊
 const app = createApp(App)
@@ -51,4 +57,5 @@ app.use(createPinia())
   .component('VForm', Form)
   .component('VField', Field)
   .component('ErrorMessage', ErrorMessage)
+  .component("font-awesome-icon", FontAwesomeIcon)
   .mount('#app')
